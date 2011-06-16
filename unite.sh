f9d9860bb725cea7339e65c5ad7fe87129886b76
José Devezas <joseluisdevezas@gmail.com>
2011-06-16 17:59:30 +0100

    Corrected the jar path to run unite.

--- unite.sh
+++ unite.sh
@@ -2,4 +2,4 @@
 
 JAVA_OPTS="-server -Xms1G -Xmx1G -XX:+AggressiveOpts -XX:CompileThreshold=200 -XX:+UseConcMarkSweepGC -Dfile.encoding=utf8"
 
-java $JAVA_OPTS -jar target/Unite-0.1-SNAPSHOT-jar-with-dependencies.jar $@
+java $JAVA_OPTS -jar target/unite-0.1-SNAPSHOT-jar-with-dependencies.jar $@
