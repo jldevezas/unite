2b9835628b044cc5fb204692c57dc4696aa84d28
José Devezas <joseluisdevezas@gmail.com>
2011-06-20 11:11:06 +0100

    Added a ProgressTracker to RDFBatchLoader.

--- src/main/java/com/josedevezas/unite/extraction/util/RDFBatchLoader.java
+++ src/main/java/com/josedevezas/unite/extraction/util/RDFBatchLoader.java
@@ -1,29 +1,35 @@
 package com.josedevezas.unite.extraction.util;
 
 import com.josedevezas.unite.util.Logging;
+import com.josedevezas.unite.util.Trackable;
+import com.josedevezas.unite.util.ProgressTracker;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.io.IOException;
 
+import java.util.List;
+import java.util.LinkedList;
+import java.util.concurrent.TimeUnit;
+
 import org.apache.commons.vfs.VFS;
 import org.apache.commons.vfs.FileObject;
 import org.apache.commons.vfs.FileSelector;
 import org.apache.commons.vfs.FileSelectInfo;
 import org.apache.commons.vfs.FileType;
-import org.apache.commons.vfs.FileSystemManager;
 import org.apache.commons.vfs.FileSystemException;
 import org.apache.log4j.Logger;
 
 import com.tinkerpop.blueprints.pgm.impls.sail.impls.NativeStoreSailGraph;
 
-public class RDFBatchLoader
+public class RDFBatchLoader implements Trackable
 {
 	private Logger log;
 	private FileObject[] files;
 	private String graphDbPath;
 	private String baseURI;
+	private long filesLoaded;
 
 	public RDFBatchLoader( String vfsPath, String graphDbPath, String baseURI )
 	{
@@ -34,14 +40,18 @@ public class RDFBatchLoader
 			String baseURI, final String fileFilter )
 	{
 		log = Logging.getLogger( this.getClass().getName() );
+		filesLoaded = 0;
 		this.graphDbPath = graphDbPath;
 		this.baseURI = baseURI;
 
+		FileObject vfsFile = null;
+
 		try
 		{
-			FileSystemManager fsManager = VFS.getManager();
-			FileObject vfsFile = fsManager.resolveFile( vfsPath );
+			log.info( "Opening " + vfsPath + "." );
+			vfsFile = VFS.getManager().resolveFile( vfsPath );
 
+			log.info( "Finding files that match the provided filter." );
 			files = vfsFile.findFiles( new FileSelector() {
 				public boolean includeFile( FileSelectInfo fileInfo ) throws FileSystemException {
 					if ( fileInfo.getFile().getType() == FileType.FILE
@@ -62,10 +72,26 @@ public class RDFBatchLoader
 		{
 			log.error( e );
 		}
+		finally
+		{
+			try
+			{
+				if ( vfsFile != null )
+				{
+					log.info( "Closing " + vfsPath + "." );
+					vfsFile.close();
+				}
+			}
+			catch ( FileSystemException e )
+			{
+				log.error( e );
+			}
+		}
 	}
 
 	public void loadFiles()
 	{
+		ProgressTracker tracker = new ProgressTracker( 30, TimeUnit.SECONDS, this );
 		NativeStoreSailGraph g = null;
 
 		try
@@ -77,7 +103,10 @@ public class RDFBatchLoader
 				InputStream inputStream = files[ i ].getContent().getInputStream();
 				g.loadRDF( inputStream, baseURI, "rdf-xml", null );
 				inputStream.close();
+				filesLoaded++;
 			}
+
+			tracker.stop();
 		}
 		catch ( Exception e )
 		{
@@ -87,5 +116,10 @@ public class RDFBatchLoader
 		{
 			if ( g != null ) g.shutdown();
 		}
+	}
+
+	public void track()
+	{
+		log.info( filesLoaded + " out of " + files.length + " files loaded so far." );
 	}
 }
